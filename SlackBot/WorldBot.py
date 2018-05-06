fa73b6008d1755c23e29fe7c765bf6a12a71705f
DESKTOP-O8JC223\YS-PC <ysck73@naver.com>
2018-05-06 20:50:08 +0900

    SlackBot 경로에 파일 추가 및 readme 수정.

---
+++ SlackBot/WorldBot.py
@@ -0,0 +1,95 @@
+import asyncio
+import websockets
+import json
+import os
+from slacker import Slacker
+
+import Commands as cmds
+
+
+class WorldBot:
+
+    def __init__(self):
+        self.m_token = os.environ["SLACK_BOT_TOKEN"]
+        self.slack = Slacker(self.m_token)
+
+        cmds.worldBot = self
+
+        # members of SlackTalker
+        self.websocket = None
+        self.target_channel = ''
+        self.target_user = ''
+        # ...
+        response = self.slack.rtm.start()
+
+        if not response.successful:
+            print("Connection Failed")
+            return;
+
+        self.bot_id = response.body['self'].get('id')
+        self.sock_endpoint = response.body['url']
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        asyncio.get_event_loop().run_until_complete(self.execute_bot())
+        asyncio.get_event_loop().run_forever()
+
+    # parameters
+    # message_json: message from slack. it must be a json type.
+    def is_callme(self, message_json):
+        return (message_json.get('type') == 'message') and (
+            message_json.get('text').startswith('<@{}>'.format(self.bot_id)))
+
+    # def msg_recv(self, text):
+    #   print(text)
+
+    # json text를 가공하여 명령어 부분만 넘긴다.
+    def command_parse(self, message):
+        msg_arr = message.split(' ', 2)
+        if len(msg_arr) <= 1:
+            return False
+
+        # 전처리 해주고(에러 잡기)
+        # ex) 문자열이 '/WAI' 가 아니라, "/WAI"라고 들어오는 걸 하나로 통일하기 등.
+        # 그 후에...
+        return msg_arr[1:]
+
+    async def execute_bot(self):
+        self.websocket = await websockets.connect(self.sock_endpoint)
+        while True:
+            message_json = await self.websocket.recv()
+            message_json = json.loads(message_json)
+            if self.is_callme(message_json):  # Write something if users call you(Bot)
+                print('Hey, Somebody calls you.')
+                print(message_json)
+
+                # 임시 방편. 봇을 언급한 메시지가 날아오면 해당 json 내의 channel과 user로 데이터를 업데이트 해놓음.
+                # slack으로 데이터를 보내는데에 사용됨.
+                self.target_channel = message_json.get('channel')
+                self.target_user = message_json.get('user')
+
+                print(self.target_channel)
+                print(self.target_user)
+                # ...
+
+                # Is a command? if yes...
+                parsed_command = self.command_parse(message_json.get('text'))
+                if parsed_command:
+                    cmds.find_command(parsed_command)
+
+
+    # @asyncio.coroutine
+    def msg_to_slack(self, text, attachments=None):
+        #yield from self.websocket.send(json.dumps({'source_team': 'T9JJGHKQX', 'user': 'U9JJGHKV5', 'text': 'Hello', 'channel': 'CAJ5CTY11', 'type': 'message', 'ts': '1525597125.000032', 'team': 'T9JJGHKQX'}))
+        #yield from self.websocket.send(json.dumps({"id": 1, "type": "message", "channel": self.target_channel,
+                                                   #"text": text}))
+        self.slack.chat.post_message(channel='#general', text='', attachments=attachments, username='worldbot_test')
+        #self.slack.chat.post_message(channel='#development_test', text='test', username='worldbot_test')
+        # if attachments:
+        #    self.slack.chat.post_message(channel=channel, text=text, attachments=attachments, username=username)
+        # else:
+        #    self.slack.chat.post_message(channel=channel, text=text, username=username)
+
+
+if __name__ == "__main__":
+    worldBot = WorldBot();
---
+++ SlackBot/readme.md
@@ -0,0 +1,27 @@
+> 아직 배포할 생각 없어서 혼자만 알아볼 수 있게 짜놓음. 첫 파이썬 프로젝트라 엉망.
+
+추가 설치된 패키지
+
+slacker, websockets, requests
+
+# 구성
+- WorldBot.py
+- SlackTalker.py
+    - Commands.py
+- WAITalker.py
+
+# 개발 상태
+현재는 WorldBot내에 SlackTalker에서 해야할 기능을 구현해놨다. 직접 메시지를 받아버리는 상태. 하지만 슬랙과 통신하는 부분은 모두 뺄 생각.
+
+Commands에 있는 세부 명령은 현재 동작하지 않는 상태. /WAI 만 가능.
+
+# 이슈 보고
+팀 내에서 사용중인 Mantis 서비스의 World Applications Info 항목에 등록하면 됨.
+
+# Commands
+**사용법**
+> /명령어 세부명령
+
+
+
+
