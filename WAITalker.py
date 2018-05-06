8ccb1709b014973044ee2aea8afb19ae31c5c65f
DESKTOP-O8JC223\YS-PC <ysck73@naver.com>
2018-05-06 20:49:37 +0900

    파일 경로를 / 에서 /SlackBot으로 옮김.

--- WAITalker.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-# JSON 데이터를 수신 후 가공하여 반환
-def recv_wai_datas():
-    r = requests.get('http://13.124.168.174:9994/Wai_interface.php'); # 링크는 외부 파일로 빼자.
-    list_data = json.loads(r.text);
-
-    return list_data;
